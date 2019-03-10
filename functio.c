182cff2be1eaba88cdaba47f4080e02d88ad76fb
honeypigs <1002213567@qq.com>
2019-03-10 16:53:27 +0800

    function

--- functio.c
+++ functio.c
@@ -21,16 +21,23 @@ int interation(float a ) {
 	x2 = 1.0;
 	do{
 		x1 = x2 ;
-		x2 = (x1 + a / x1) / 2;
+		x2 = (x1 + a / x1) / 2; 
 	} while(fabs(x1-x2)>pow(10,-5));
 	printf("%lf\n",x2);
 	return 0;
 }
 
+void function(char * p1,char * p2) {
+	printf("12");
 
+}
 
 int main () {
+	char * p1= "abcde",* p2 = "1234567";
+	printf("%s",p1);
+	function(p1,p2);
+	printf("%s\n%s\n",p1,p2);
 	// hanoi(3,'A','B','C');
-	interation(6);
+	// interation(4);
 	return 0;
 }
